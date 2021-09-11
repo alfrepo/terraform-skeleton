6d7c7fce760a93b4d5b572d63ab27c23101695eb
anena <anenada@gmail.com>
2021-09-11 21:49:37 +0200

    init

---
+++ example_resource_thing_policy.tf
@@ -0,0 +1,4 @@
+resource "aws_iot_policy" "iot_policy" {
+  name   = "${local.iot_policy}"
+  policy = "${data.template_file.tf_iot_policy.rendered}"
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources into"
+  type = string
+  default = "eu-central-1"
+}
+
+variable "aws_account_id" {
+  description = "The AWS account identifier of the project"
+  type = string
+  default = "453267903880"
+}
+
+variable "prefix" {
+  description = "The resource prefix"
+  type = string
+  default = "alf-dev-con1"
+}
+
+
+locals {
+  iot_policy = "${var.prefix}-thing2"
+}
