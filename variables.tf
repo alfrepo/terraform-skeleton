e20ff8a1625f6fed8bea1a1a43527cb78aacf87a
anena <anenada@gmail.com>
2021-09-11 22:06:12 +0200

    acc

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "aws_account_id" {
   description = "The AWS account identifier of the project"
   type = string
-  default = "453267903880"
+  default = "1234567891234"
 }
 
 variable "prefix" {
