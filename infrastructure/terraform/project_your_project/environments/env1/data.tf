5169f17fd13634e3aaf8417336859cd099065dab
Alex <3707177+skipidar@users.noreply.github.com>
2023-11-28 19:16:24 +0100

    adding parameter store

--- infrastructure/terraform/project_your_project/environments/env1/data.tf
+++ infrastructure/terraform/project_your_project/environments/env1/data.tf
@@ -4,3 +4,16 @@
 #
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
+
+# import the AWS parameter store variables from the environment
+# which you have deployed there during bootstrap
+# 12-factor-app store config in environment
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "${local.prefix_shared_platform}/public_subnet_ids"  # Use the same path used for storage
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "${local.prefix_shared_platform}/private_subnet_ids"  # Use the same path used for storage
+}
--- infrastructure/terraform/project_your_project/environments/env1/variables.tf
+++ infrastructure/terraform/project_your_project/environments/env1/variables.tf
@@ -29,5 +29,24 @@ locals {
 
   # make also adressable via "var."
   prefix = "${var.prefix}"
-  environment = "${var.environment}"     
+  environment = "${var.environment}"
+
+  # getting the  shared variables from environment, from the AWS parameter store
+
+  # example of storing the param:
+  # assuming that you stored  the variables under this prefix
+  # resource "aws_ssm_parameter" "vpc_parameter_public_subnet_ids" {
+  #   name  = "/${var.prefix}/${var.environment}/public_subnet_ids"
+  #   type  = "StringList"
+  #   value = jsonencode(module.vpc.public_subnets)
+  # }
+
+  prefix_shared_platform = "/alf-dev/shr1"
+  public_subnet1_arn = jsondecode(data.aws_ssm_parameter.public_subnet_arns.value)[0]
+  public_subnet2_arn = jsondecode(data.aws_ssm_parameter.public_subnet_arns.value)[1]
+  public_subnet3_arn = jsondecode(data.aws_ssm_parameter.public_subnet_arns.value)[2]
+
+  public_subnet1_id = jsondecode(data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  public_subnet2_id = jsondecode(data.aws_ssm_parameter.public_subnet_ids.value)[1]
+  public_subnet3_id = jsondecode(data.aws_ssm_parameter.public_subnet_ids.value)[2]
 }
